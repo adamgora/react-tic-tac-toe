a9ab7015dca3b5ed959122b721a3246f5dd98f5b
Adam Góra <adamgora@icloud.com>
2018-02-03 10:06:04 +0100

    Board rendering

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# react-tic-tac-toe
+# react-tic-tac-toe (work in progress)
 Simple Tic Tac Toe game with AI, built with ReactJS
--- src/App.js
+++ src/App.js
@@ -1,21 +1,49 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor() {
+
+        super();
+        this.state = {
+            board: [
+                ['O', null, 'X'],
+                ['X', null, null],
+                ['X', 'O', 'O']
+            ],
+            board_locked: false,
+            game_over: false,
+            current_player: 0,
+            players: ['X', 'O'],
+            ai_player: 'X',
+            human_player: 'O',
+        }
+    }
+
+    render() {
+        return (
+            <div id='game-board'>
+                {this.state.board.map((row, rindex) => {
+                    return (
+                        <div className="row" key={rindex}>
+                            {row.map((cell, cindex) => {
+                                return (
+                                    <div className="cell"
+                                         key={cindex}
+                                         onClick={() => this.handleCellClick(rindex, cindex)}
+                                    >
+                                        {cell}
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    )
+                })}
+
+                <p>Current player is {this.state.players[this.state.current_player]}</p>
+            </div>
+        );
+    }
 }
 
 export default App;
