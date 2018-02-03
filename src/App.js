4237f2abbe9139a5c7fced6c1e07a3761e5ece31
Adam Góra <adamgora@icloud.com>
2018-02-03 10:22:51 +0100

    Determining winner of the game and points for the future minimax algorithm

--- src/App.js
+++ src/App.js
@@ -20,6 +20,109 @@ class App extends Component {
         }
     }
 
+    runAI() {
+        let ai = this.minimax(this.clone(this.state.board));
+        console.log(ai);
+    }
+
+    score(game) {
+        const result = this.checkWinner(game);
+        let data = {
+            game_over: result.game_over,
+            score: 0
+        };
+
+        if(result.winner === this.state.aiPlayer) {
+            data.score = 10;
+        } else if (result.winner === this.state.humanPlayer) {
+            data.score = -10
+        }
+
+        return data;
+    }
+
+    minimax(game) {
+        const score = this.score(game);
+        if(score.game_over) {
+            return score.score;
+        }
+
+        return score;
+    }
+
+    getNewState(game, move, player) {
+
+    }
+
+    getAvailableMoves(game) {
+
+    }
+
+    handleCellClick() {
+
+    }
+
+    clone(obj) {
+        return JSON.parse(JSON.stringify(obj));
+    }
+
+    checkWinner(board) {
+        let winner = '';
+        let result = {
+            winner: '',
+            game_over: false,
+            is_tie: false,
+        };
+
+        // check all rows
+        for(let row of board) {
+            const distinct = Array.from(new Set([...row]));
+            if (distinct.length === 1 && distinct[0]) {
+                winner = distinct[0];
+            }
+        }
+
+        // check all columns
+        for(let i = 0; i < 3; i++) {
+            let values = [];
+            for(let row of board) {
+                values.push(row[i]);
+            }
+            const distinct = Array.from(new Set([...values]));
+            if (distinct.length === 1 && distinct[0]) {
+                winner = distinct[0];
+            }
+        }
+
+        // check diagonals
+        if(board[0][0] === board[1][1] && board[0][0] === board[2][2] && board[0][0]) {
+            winner = board[0][0];
+        }
+        if(board[0][2] === board[1][1] && board[0][2] === board[2][0] && board[0][2]) {
+            winner = board[0][2];
+        }
+
+        // check if tie
+        let flattened = [].concat.apply([], board);
+        if(!winner && !~flattened.indexOf(null)) {
+            result.is_tie = true;
+        }
+
+        if(winner) {
+            console.log(`Winner is: ${winner}`);
+            result.winner = winner;
+            result.game_over = true;
+        }
+
+        if(result.is_tie) {
+            console.log(`Game ended with a tie!`);
+            result.game_over = true;
+        }
+
+        return result;
+
+    }
+
     render() {
         return (
             <div id='game-board'>
@@ -39,8 +142,9 @@ class App extends Component {
                         </div>
                     )
                 })}
-
                 <p>Current player is {this.state.players[this.state.current_player]}</p>
+
+                <button onClick={() => this.runAI()}>Run AI</button>
             </div>
         );
     }
